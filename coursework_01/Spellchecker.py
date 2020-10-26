6306bd14348dd32613c2d551d0fd19acfeb4fc63
t11915jr <jeremy.roe@student.manchester.ac.uk>
2020-10-26 17:56:10 +0000

    Added menu

--- coursework_01/Spellchecker.py
+++ coursework_01/Spellchecker.py
@@ -1,8 +1,32 @@
+# Modules to be imported
+import sys
+
+
+# Class to format printed text
+class Format:
+    end = '\033[0m'
+    underline = '\033[4m'
+
+
+# Main menu
 def menu():
-    user_input = input("Spellchecker by Jeremy Roe \n" +
-                       "Menu page:\n\n"+
-                       "1:\tSpell check sentence\n"+
-                       "2:\tSpell check file\n"+
-                       "0:\tQuit program")
+    choice_made = False
+    print("Spellchecker by Jeremy Roe")
+    while not choice_made:
+        user_input = input(Format.underline + "Menu page\n\n" + Format.end +
+                           "\t1:\tSpell check sentence\n" +
+                           "\t2:\tSpell check file\n" +
+                           "\t0:\tQuit program\n")
+
+        if int(user_input) == 1:
+            pass
+        elif int(user_input) == 2:
+            pass
+        elif int(user_input) == 0:
+            print("Spellchecker closed")
+            sys.exit()
+        else:
+            print("invalid choice, try again")
+
 
 menu()
