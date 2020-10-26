163556c6dbb4cde003070b366b417b231446a451
t11915jr <jeremy.roe@student.manchester.ac.uk>
2020-10-26 17:40:54 +0000

    Setup Coursework

---
+++ coursework_01/Spellchecker.py
@@ -0,0 +1,8 @@
+def menu():
+    user_input = input("Spellchecker by Jeremy Roe \n" +
+                       "Menu page:\n\n"+
+                       "1:\tSpell check sentence\n"+
+                       "2:\tSpell check file\n"+
+                       "0:\tQuit program")
+
+menu()
